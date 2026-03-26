85f11d6acd344a93e0abccab60092810f96b331c
Santiago Acosta <santiagoacosta777@gmail.com>
2026-03-26 14:46:33 -0300

    Fix herramientas dev CSRF bypass

--- web/herramientas/routes/web.php
+++ web/herramientas/routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Http\Controllers\HerramientasController;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Foundation\Http\Middleware\PreventRequestForgery;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HerramientasController::class, 'index'])->name('home');
 
 Route::post('/api/tools/consulta-renovacion-cruz-del-eje', [HerramientasController::class, 'consultaRenovacionCruzDelEje'])
-    ->withoutMiddleware([VerifyCsrfToken::class])
+    ->withoutMiddleware([PreventRequestForgery::class])
     ->name('tools.consulta-renovacion-cruz-del-eje');
